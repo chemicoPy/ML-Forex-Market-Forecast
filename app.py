ba1ea1cd362b6920574db582ad26e9ee766f4405
Victor Ogunjobi <66210738+chemicoPy@users.noreply.github.com>
2022-12-16 01:15:19 +0100

    Update app.py

--- app.py
+++ app.py
@@ -333,3 +333,192 @@ class LSTMModel(nn.Module):
 
 model = LSTMModel(input_size=config["model"]["input_size"], hidden_layer_size=config["model"]["lstm_size"], num_layers=config["model"]["num_lstm_layers"], output_size=1, dropout=config["model"]["dropout"])
 model = model.to(config["training"]["device"])
+
+
+# ---------------Model Training------------------
+
+def run_epoch(dataloader, is_training=False):
+    epoch_loss = 0
+
+    if is_training:
+        model.train()
+    else:
+        model.eval()
+
+    for idx, (x, y) in enumerate(dataloader):
+        if is_training:
+            optimizer.zero_grad()
+
+        batchsize = x.shape[0]
+
+        x = x.to(config["training"]["device"])
+        y = y.to(config["training"]["device"])
+
+        out = model(x)
+        loss = criterion(out.contiguous(), y.contiguous())
+
+        if is_training:
+            loss.backward()
+            optimizer.step()
+
+        epoch_loss += (loss.detach().item() / batchsize)
+
+    lr = scheduler.get_last_lr()[0]
+
+    return epoch_loss, lr
+
+# create `DataLoader`
+train_dataloader = DataLoader(dataset_train, batch_size=config["training"]["batch_size"], shuffle=True)
+val_dataloader = DataLoader(dataset_val, batch_size=config["training"]["batch_size"], shuffle=True)
+
+# define optimizer, scheduler and loss function
+criterion = nn.MSELoss()
+optimizer = optim.Adam(model.parameters(), lr=config["training"]["learning_rate"], betas=(0.9, 0.98), eps=1e-9)
+scheduler = optim.lr_scheduler.StepLR(optimizer, step_size=config["training"]["scheduler_step_size"], gamma=0.1)
+
+loss_value = []
+epochs  = []
+training_loss = []
+validation_loss = []
+
+# begin training
+for epoch in range(config["training"]["num_epoch"]):
+    loss_train, lr_train = run_epoch(train_dataloader, is_training=True)
+    loss_val, lr_val = run_epoch(val_dataloader)
+    scheduler.step()
+    
+    print('Epoch[{}/{}] | loss train:{:.6f}, test:{:.6f} | lr:{:.6f}'
+              .format(epoch+1, config["training"]["num_epoch"], loss_train, loss_val, lr_train))
+    
+    loss_value.append(loss_val)
+    epochs.append(epoch)
+    training_loss.append(loss_train)
+    validation_loss.append(loss_val)
+
+    
+# ---------------Model Evaluation------------------
+    
+# here i re-initialize dataloader so the data isn't shuffled, so we can plot the values by date
+
+train_dataloader = DataLoader(dataset_train, batch_size=config["training"]["batch_size"], shuffle=False)
+val_dataloader = DataLoader(dataset_val, batch_size=config["training"]["batch_size"], shuffle=False)
+
+model.eval()
+
+# predict on the training data, to see how well the model managed to learn and memorize
+
+predicted_train = np.array([])
+
+for idx, (x, y) in enumerate(train_dataloader):
+    x = x.to(config["training"]["device"])
+    out = model(x)
+    out = out.cpu().detach().numpy()
+    predicted_train = np.concatenate((predicted_train, out))
+
+# predict on the validation data, to see how the model does
+
+predicted_val = np.array([])
+
+for idx, (x, y) in enumerate(val_dataloader):
+    x = x.to(config["training"]["device"])
+    out = model(x)
+    out = out.cpu().detach().numpy()
+    predicted_val = np.concatenate((predicted_val, out))
+
+if config["plots"]["show_plots"]:
+
+    # prepare data for plotting, show predicted prices
+
+    to_plot_data_y_train_pred = np.zeros(num_data_points)
+    to_plot_data_y_val_pred = np.zeros(num_data_points)
+
+    to_plot_data_y_train_pred[config["data"]["window_size"]:split_index+config["data"]["window_size"]] = scaler.inverse_transform(predicted_train)
+    to_plot_data_y_val_pred[split_index+config["data"]["window_size"]:] = scaler.inverse_transform(predicted_val)
+
+    to_plot_data_y_train_pred = np.where(to_plot_data_y_train_pred == 0, None, to_plot_data_y_train_pred)
+    to_plot_data_y_val_pred = np.where(to_plot_data_y_val_pred == 0, None, to_plot_data_y_val_pred)
+
+    # plots
+
+    fig = figure(figsize=(25, 5), dpi=80)
+    fig.patch.set_facecolor((1.0, 1.0, 1.0))
+    plt.plot(data_date, data_close_price, label="Actual prices", color=config["plots"]["color_actual"])
+    plt.plot(data_date, to_plot_data_y_train_pred, label="Predicted prices (train)", color=config["plots"]["color_pred_train"])
+    plt.plot(data_date, to_plot_data_y_val_pred, label="Predicted prices (validation)", color=config["plots"]["color_pred_val"])
+    plt.title("Compare predicted prices to actual prices")
+    xticks = [data_date[i] if ((i%config["plots"]["xticks_interval"]==0 and (num_data_points-i) > config["plots"]["xticks_interval"]) or i==num_data_points-1) else None for i in range(num_data_points)] # make x ticks nice
+    x = np.arange(0,len(xticks))
+    plt.xticks(x, xticks, rotation='vertical')
+    plt.grid(b=None, which='major', axis='y', linestyle='--')
+    plt.legend()
+    plt.show()
+
+    # prepare data for plotting, zoom in validation
+
+    to_plot_data_y_val_subset = scaler.inverse_transform(data_y_val)
+    to_plot_predicted_val = scaler.inverse_transform(predicted_val)
+    to_plot_data_date = data_date[split_index+config["data"]["window_size"]:]
+
+    # plots
+
+    fig = figure(figsize=(25, 5), dpi=80)
+    fig.patch.set_facecolor((1.0, 1.0, 1.0))
+    plt.plot(to_plot_data_date, to_plot_data_y_val_subset, label="Actual prices", color=config["plots"]["color_actual"])
+    plt.plot(to_plot_data_date, to_plot_predicted_val, label="Predicted prices (validation)", color=config["plots"]["color_pred_val"])
+    plt.title("Zoom in to examine predicted price on validation data portion")
+    xticks = [to_plot_data_date[i] if ((i%int(config["plots"]["xticks_interval"]/5)==0 and (len(to_plot_data_date)-i) > config["plots"]["xticks_interval"]/6) or i==len(to_plot_data_date)-1) else None for i in range(len(to_plot_data_date))] # make x ticks nice
+    xs = np.arange(0,len(xticks))
+    plt.xticks(xs, xticks, rotation='vertical')
+    plt.grid(b=None, which='major', axis='y', linestyle='--')
+    plt.legend()
+    plt.show()
+    
+    
+    
+# ---------------Predicting future forex prices------------------  
+# predict on the unseen data, next price 
+
+model.eval()
+
+x = torch.tensor(data_x_unseen).float().to(config["training"]["device"]).unsqueeze(0).unsqueeze(2) # this is the data type and shape required, [batch, sequence, feature]
+prediction = model(x)
+prediction = prediction.cpu().detach().numpy()
+prediction = scaler.inverse_transform(prediction)[0]
+
+if config["plots"]["show_plots"]:
+        
+    # prepare plots
+
+    plot_range = 10
+    to_plot_data_y_val = np.zeros(plot_range)
+    to_plot_data_y_val_pred = np.zeros(plot_range)
+    to_plot_data_y_test_pred = np.zeros(plot_range)
+
+    to_plot_data_y_val[:plot_range-1] = scaler.inverse_transform(data_y_val)[-plot_range+1:]
+    to_plot_data_y_val_pred[:plot_range-1] = scaler.inverse_transform(predicted_val)[-plot_range+1:]
+
+    to_plot_data_y_test_pred[plot_range-1] = prediction
+
+    to_plot_data_y_val = np.where(to_plot_data_y_val == 0, None, to_plot_data_y_val)
+    to_plot_data_y_val_pred = np.where(to_plot_data_y_val_pred == 0, None, to_plot_data_y_val_pred)
+    to_plot_data_y_test_pred = np.where(to_plot_data_y_test_pred == 0, None, to_plot_data_y_test_pred)
+    
+    # plot
+
+    plot_date_test = data_date[-plot_range+1:]
+    plot_date_test.append("next trading time")
+
+    fig = figure(figsize=(25, 5), dpi=80)
+    fig.patch.set_facecolor((1.0, 1.0, 1.0))
+    plt.plot(plot_date_test, to_plot_data_y_val, label="Actual prices", marker=".", markersize=10, color=config["plots"]["color_actual"])
+    plt.plot(plot_date_test, to_plot_data_y_val_pred, label="Past predicted prices", marker=".", markersize=10, color=config["plots"]["color_pred_val"])
+    plt.plot(plot_date_test, to_plot_data_y_test_pred, label="Predicted price for next time", marker=".", markersize=20, color=config["plots"]["color_pred_test"])
+    plt.title("Predicted close price of the next trading time")
+    plt.grid(b=None, which='major', axis='y', linestyle='--')
+    plt.legend()
+    plt.show()
+    
+    st.subheader("Predicting future forex prices")
+    st.write(fig)
+
+st.write("Predicted close price of the next trading time:", round(prediction, 2))
